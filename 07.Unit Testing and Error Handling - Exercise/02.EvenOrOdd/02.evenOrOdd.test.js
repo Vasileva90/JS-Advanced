dbe771a4ad9f7a4bdcc4e7d8d77a27385b91c143
Vasileva90 <107758821+Vasileva90@users.noreply.github.com>
2023-03-14 11:20:09 +0200

    Add tasks

---
+++ 07.Unit Testing and Error Handling - Exercise/02.EvenOrOdd/02.evenOrOdd.test.js
@@ -0,0 +1,28 @@
+const isOddOrEven = require('../02.evenOrOdd');
+const { assert } = require('chai');
+
+describe('isOddOrEven function tests', () => {
+    //invalid input tests
+    it('should return undefined if the parameter is number', () => {
+        assert.equal(isOddOrEven(10),undefined);
+    });
+    it('should return undefined if the parameter is object', () => {
+        assert.equal(isOddOrEven({}),undefined);
+    });
+    it('should return undefined if the parameter is array', () => {
+        assert.equal(isOddOrEven([]),undefined);
+    });
+    it('should return undefined if the parameter is undefined', () => {
+        assert.equal(isOddOrEven(undefined),undefined);
+    });
+    it('should return undefined if the parameter is null', () => {
+        assert.equal(isOddOrEven(null),undefined);
+    });
+    //valid input tests
+    it('should return even as result', () => {
+        assert.equal(isOddOrEven('Hi'),'even');
+    });
+    it('should return odd as result', () => {
+        assert.equal(isOddOrEven('Hello'),'odd');
+    });
+});
